a3146da82e5eeee3672a4e7ed3857129cd0db11c
Hidde Makimei <hidde@siip.group>
2024-06-12 15:33:00 +0200

    setting up structure

---
+++ quiz/main/src/main/java/org/example/Question.java
@@ -0,0 +1,19 @@
+public class Question {
+    private int id;
+    private String question;
+    private String op1;
+    private String op2;
+    private String op3;
+    private String op4;
+    private String answer;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
